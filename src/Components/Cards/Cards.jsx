155075e97ac1cd1757d5a623c768cdddc0b99398
Mohit Bisht <51907464+Mohitb07@users.noreply.github.com>
2020-08-16 17:49:44 +0530

    Initial commit

---
+++ src/Components/Cards/Cards.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+import { Card, CardContent, Typography, Grid } from '@material-ui/core';
+import styles from './Cards.module.css'
+
+const Info = ({data:{confirmed, recovered, deaths, lastUpdated}}) => {
+
+    if(!confirmed){
+        console.log('still loading',confirmed)
+        return 'Loading'
+    }
+
+    return (            
+        <div className={styles.container}>
+            <Grid container spacing={3} justify="center">
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Infected</Typography>
+                        <Typography variant="h5">{confirmed.value}</Typography>
+                        <Typography color="textSecondary" >Real date</Typography>
+                        <Typography variant="body2">Number of active cases of COVID-19</Typography>
+                    </CardContent>
+                </Grid>
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Recovered</Typography>
+                        <Typography variant="h5">Real Data</Typography>
+                        <Typography color="textSecondary" >Real date</Typography>
+                        <Typography variant="body2">Number of recovered cases of COVID-19</Typography>
+                    </CardContent>
+                </Grid>
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Deaths</Typography>
+                        <Typography variant="h5">Real Data</Typography>
+                        <Typography color="textSecondary" >Real date</Typography>
+                        <Typography variant="body2">Number of deaths caused by COVID-19</Typography>
+                    </CardContent>
+                </Grid>
+            </Grid>
+        </div>    
+    )
+}
+
+
+export default Info;
