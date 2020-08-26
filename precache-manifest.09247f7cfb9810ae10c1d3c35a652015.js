827153aa7782ab3d568839dd85b699cdfd9333c1
Mohit Bisht <51907464+Mohitb07@users.noreply.github.com>
2020-08-26 16:04:34 +0530

    Updates

---
+++ precache-manifest.09247f7cfb9810ae10c1d3c35a652015.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0abc602101ff5780b5db5811209bf5b1",
+    "url": "/corona-tracker/index.html"
+  },
+  {
+    "revision": "d16ea33b2626b595a24a",
+    "url": "/corona-tracker/static/css/main.6723e153.chunk.css"
+  },
+  {
+    "revision": "fbe1944889c21d5ee00f",
+    "url": "/corona-tracker/static/js/2.639b72e9.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/corona-tracker/static/js/2.639b72e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d16ea33b2626b595a24a",
+    "url": "/corona-tracker/static/js/main.428fd5de.chunk.js"
+  },
+  {
+    "revision": "0fe3db4d939fef099a91",
+    "url": "/corona-tracker/static/js/runtime-main.e3654df2.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/corona-tracker/static/media/covid2.d7265326.png"
+  }
+]);
